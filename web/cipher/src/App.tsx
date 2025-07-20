321a763bfb01d0b990e07c71e88313f1fe387350
dangvu2408 <hiend6887@gmail.com>
2025-07-20 22:36:05 +0700

    a

--- web/cipher/src/App.tsx
+++ web/cipher/src/App.tsx
@@ -7,7 +7,13 @@ function App() {
     <>
       <div className='title'>Mã hóa/Giải mã các hệ mật</div>
       <div className="card">
-        
+        <form className='form_inpt'>
+          <div className='column'>
+            <div className='row'>
+              <input type="text" placeholder="Nhập string cần mã hóa" className="form_input_encrypt" id="encrypt_str"></input>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   )
